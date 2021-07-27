f57b37f143bdbb265d15294af682c3f3cbd305f2
Casey Cameron <mcasey.cameron@gmail.com>
2021-07-27 16:49:02 -0700

    List Rubric complete

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import AvatarCharacters from '../../containers/Characters';
 
 export default function App() {
+  //List Rubric complete
   return <AvatarCharacters />;
 }
