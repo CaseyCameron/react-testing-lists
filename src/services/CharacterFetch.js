0dc9e1e7712b93578e0cdb409511925f9ab02da8
Casey Cameron <mcasey.cameron@gmail.com>
2021-07-27 17:07:55 -0700

    Setup - fetchCharacterById

--- src/services/CharacterFetch.js
+++ src/services/CharacterFetch.js
@@ -3,3 +3,9 @@ export const fetchCharacters = async () => {
   const json = await res.json();
   return json;
 }
+
+export const fetchCharactersById = async (id) => {
+  const res = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters${id}`);
+  const json = await res.json();
+  return json;
+}
